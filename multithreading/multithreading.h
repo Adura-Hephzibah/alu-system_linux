67ae0f6e5e585bd9b067a763f1f231a4dd58ae75
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-03-29 15:08:20 +0200

    task 5

--- multithreading/multithreading.h
+++ multithreading/multithreading.h
@@ -78,5 +78,6 @@ int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 int tprintf(char const *format, ...);
+list_t *prime_factors(char const *s);
 
 #endif
