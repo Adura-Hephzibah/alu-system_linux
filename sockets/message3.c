f80a043c8eac3cc4b2f947d378940c1e88f6c52f
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-04-05 18:43:05 +0200

    try and work

--- sockets/message3.c
+++ sockets/message3.c
@@ -2,12 +2,15 @@
 
 void requst_breakdown_printout(void *message)
 {
-	char *path = NULL, *line = NULL, *data = NULL;
-	char *brk = "\r\n", *save_ptr1 = NULL, *save_ptr2 = NULL, *key = NULL, *value = NULL;
+	char *path = NULL, *line = NULL, *method = NULL, *all_data = NULL, *data = NULL;
+	char *brk = "\r\n", *req_type = NULL, *save_ptr1 = NULL, *save_ptr2 = NULL, *key = NULL, *value = NULL;
 	char *body = NULL;
 
 	line = strtok_r(message, brk, &save_ptr1);
+	method = strtok_r(line, " ", &save_ptr2);
 	path = strtok_r(NULL, " ", &save_ptr2);
+	all_data = strtok_r(NULL, " ", &save_ptr2);
+	req_type = strtok_r(NULL, "\r\n", &save_ptr2);
 	printf("Path: %s\n", path);
 
 	while ((line = strtok_r(NULL, brk, &save_ptr1)) && strlen(line) > 0);
