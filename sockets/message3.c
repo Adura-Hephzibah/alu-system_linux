a15ee27c7d0eff6d8137622756f4e2c426121a73
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-04-05 18:36:36 +0200

    need to stop now

--- sockets/message3.c
+++ sockets/message3.c
@@ -3,25 +3,25 @@
 void requst_breakdown_printout(void *message)
 {
 	char *path = NULL, *line = NULL, *data = NULL;
-	char *brk = "\r\n", *save_ptr = NULL, *key = NULL, *value = NULL;
+	char *brk = "\r\n", *save_ptr1 = NULL, *save_ptr2 = NULL, *key = NULL, *value = NULL;
 	char *body = NULL;
 
-	line = strtok(message, brk);
-	path = strtok(NULL, " ");
+	line = strtok_r(message, brk, &save_ptr1);
+	path = strtok_r(NULL, " ", &save_ptr2);
 	printf("Path: %s\n", path);
 
-	while ((line = strtok(NULL, brk)) && strlen(line) > 0);
+	while ((line = strtok_r(NULL, brk, &save_ptr1)) && strlen(line) > 0);
 
-	body = strtok(NULL, "");
+	body = strtok_r(NULL, "", &save_ptr1);
 	if (body)
 	{
-		data = strtok_r(body, "&", &save_ptr);
+		data = strtok_r(body, "&", &save_ptr2);
 		while (data)
 		{
 			key = strtok(data, "=");
 			value = strtok(NULL, "=");
 			printf("Body param: \"%s\" -> \"%s\"\n", key, value);
-			data = strtok_r(NULL, "&", &save_ptr);
+			data = strtok_r(NULL, "&", &save_ptr2);
 		}
 	}
 	return;
