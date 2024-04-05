3e72bf56e48e64b446676f4e73498e6f8ef6bcf8
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-04-05 18:27:54 +0200

    tiredd

--- sockets/message3.c
+++ sockets/message3.c
@@ -4,20 +4,27 @@ void requst_breakdown_printout(void *message)
 {
 	char *path = NULL, *line = NULL, *method = NULL, *all_data = NULL, *data = NULL;
 	char *brk = "\r\n", *req_type = NULL, *save_ptr = NULL, *key = NULL, *value = NULL;
+	char *body = NULL;
 
 	line = strtok(message, brk);
 	method = strtok(line, " ");
 	path = strtok(NULL, " ");
-	all_data = strtok(NULL, " ");
 	req_type = strtok(NULL, "\r\n");
 	printf("Path: %s\n", path);
-	data = strtok_r(all_data, "&", &save_ptr);
-	while (data && method && req_type)
+
+	while ((line = strtok(NULL, brk)) && strlen(line) > 0);
+
+	body = strtok(NULL, "");
+	if (body)
 	{
-		key = strtok(data, "=");
-		value = strtok(NULL, "=");
-		printf("Body param: \"%s\" -> \"%s\"\n", key, value);
-		data = strtok_r(NULL, "&", &save_ptr);
+		data = strtok_r(body, "&", &save_ptr);
+		while (data)
+		{
+			key = strtok(data, "=");
+			value = strtok(NULL, "=");
+			printf("Body param: \"%s\" -> \"%s\"\n", key, value);
+			data = strtok_r(NULL, "&", &save_ptr);
+		}
 	}
 	return;
 }
