3208c7be140f181885fcd63c663577e09ff40a7d
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-04-05 22:53:37 +0200

    ehh

--- sockets/message3.c
+++ sockets/message3.c
@@ -1,63 +1,36 @@
 #include "sockets.h"
 
-void requst_breakdown_printout(void)
+void requst_breakdown_printout(void *message)
 {
-	char *path = NULL, *all_data = NULL, *data = NULL;
-	char *key = NULL, *value = NULL;
+	char *path = NULL, *line = NULL, *method = NULL, *all_data = NULL, *data = NULL;
+	char *brk = "\r\n", *req_type = NULL, *save_ptr = NULL, *key = NULL, *value = NULL;
+	char *body_param = NULL;
 
+	line = strtok(message, brk);
+	method = strtok(line, " ");
 	path = strtok(NULL, " ");
 
 	printf("Path: %s\n", path);
 
-	all_data = strtok(NULL, "");
-	if (all_data != NULL)
+
+	all_data = strtok(NULL, "\r\n\r\n");
+	if (all_data)
 	{
-		printf("Body params:\n");
+		all_data += strlen("\r\n");
 		data = strtok(all_data, "&");
-		while (data)
+
+		while (data && method)
 		{
 			key = strtok(data, "=");
 			value = strtok(NULL, "=");
-			printf("\"%s\" -> \"%s\"\n", key, value);
+
+			if (key && value)
+			{
+				printf("Body param: \"%s\" -> \"%s\"\n", key, value);
+			}
+
 			data = strtok(NULL, "&");
 		}
 	}
-}
-
-int main(void)
-{
-	int sockid = 0, sockid_c = 0, client_size = 0, server_size = 0;
-	struct sockaddr_in client;
-	struct sockaddr_in addrport;
-	char message[1024];
-	char response[] = "HTTP/1.1 200 OK\r\n\r\n";
-	size_t count = recv(sockid_c, message, sizeof(message), 0);
-
-	sockid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	addrport.sin_family = AF_INET;
-	addrport.sin_addr.s_addr = INADDR_ANY;
-	addrport.sin_port = htons(8080);
-	server_size = sizeof(addrport);
-	bind(sockid, (struct sockaddr *) &addrport, server_size);
-	printf("Server listening on port 8080\n");
-
-	while (1)
-	{
-		listen(sockid, 8);
-		client_size = sizeof(client);
-		sockid_c = accept(sockid, (struct sockaddr *) &client, (socklen_t *) &client_size);
-		printf("Client connected: %s\n", inet_ntoa(client.sin_addr));
-
-		if (count > 0)
-		{
-			message[count] = '\0';
-			printf("Raw request: \"%s\"\n", message);
-			send(sockid_c, response, strlen(response), 0);
-			requst_breakdown_printout((void *)message);
-		}
-
-		close(sockid_c);
-	}
-
-	return (0);
+	return;
 }
