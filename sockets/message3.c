a1deffb379ac74c89fa02456dbdfb675a3b5cc79
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-04-05 17:34:17 +0200

    hmm

---
+++ sockets/message3.c
@@ -0,0 +1,37 @@
+#include "sockets.h"
+
+void request_breakdown_printout(char *message)
+{
+	char *line = NULL, *request_line = NULL, *header_line = NULL, *body = NULL;
+	char *brk = "\r\n";
+	char *saveptr1, *saveptr2;
+
+	request_line = strtok_r(message, brk, &saveptr1);
+
+	char method[8], path[32], version[16];
+
+	sscanf(request_line, "%s %s %s", method, path, version);
+	printf("Path: %s\n", path);
+
+	header_line = strtok_r(NULL, brk, &saveptr1);
+	while (header_line && header_line[0] != '\0')
+	{
+		char key[32], value[64];
+
+		sscanf(header_line, "%[^:]: %[^\t\n]", key, value);
+		header_line = strtok_r(NULL, brk, &saveptr1);
+	}
+
+	body = strtok_r(NULL, "", &saveptr1);
+
+	char *param = strtok_r(body, "&", &saveptr2);
+
+	while (param)
+	{
+		char key[32], value[32];
+
+		sscanf(param, "%[^=]=%s", key, value);
+		printf("Body param: \"%s\" -> \"%s\"\n", key, value);
+		param = strtok_r(NULL, "&", &saveptr2);
+	}
+}
