cc8d6a2660add2bf54d904775ec7253511d58bda
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-04-05 23:10:06 +0200

    ooo

--- sockets/message3.c
+++ sockets/message3.c
@@ -4,14 +4,14 @@
 
 void requst_breakdown_printout(void *message)
 {
-	char *path = NULL, *line = NULL, *method = NULL, *all_data = NULL, *data = NULL;
+	char *path = NULL, *line = NULL, *all_data = NULL, *data = NULL;
 	char *brk = "\r\n", *key = NULL, *value = NULL;
 
 	line = strtok(message, brk);
-	method = strtok(line, " ");
 	path = strtok(NULL, " ");
 
 	printf("Path: %s\n", path);
+
 
 	all_data = strtok(NULL, "\r\n\r\n");
 	if (all_data)
@@ -19,7 +19,7 @@ void requst_breakdown_printout(void *message)
 		all_data += strlen("\r\n\r\n");
 		data = strtok(all_data, "&");
 
-		while (data && method)
+		while (data)
 		{
 			key = strtok(data, "=");
 			value = strtok(NULL, "=");
